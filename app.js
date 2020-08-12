8675809342513601bccd96292d2d3d8c71193907
Anmol Noor <anmolnoor59@gmail.com>
2020-08-12 22:48:59 +0530

    this is final commit from local side which consist procfile, gitignore , update ports

--- app.js
+++ app.js
@@ -155,6 +155,14 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port,function() {
+  console.log("Server has started Successfully");
 });
+//
+// app.listen(3000, function() {
+//   console.log("Server started on port 3000");
+// });
